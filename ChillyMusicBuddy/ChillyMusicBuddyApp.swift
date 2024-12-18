47b45819b5e396191cea1e1a1691d75bbf43904b
huyle0203 <itshuy.le@gmail.com>
2024-12-17 23:18:11 -0800

    Initial Commit

---
+++ ChillyMusicBuddy/ChillyMusicBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChillyMusicBuddyApp.swift
+//  ChillyMusicBuddy
+//
+//  Created by Huy Le on 12/17/24.
+//
+
+import SwiftUI
+
+@main
+struct ChillyMusicBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
